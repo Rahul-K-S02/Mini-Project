29e099b9285bce87aff634c5ca95f3b010072bcb
sagargkunte <sagargkunte24@gmail.com>
2025-10-24 20:20:09 +0530

    Continued the mini project

--- Doctor-Patient/routes/patient.js
+++ Doctor-Patient/routes/patient.js
@@ -1,10 +1,28 @@
 import { Router } from "express";
+import { doctor } from "../models/doctor.js";
+import passport from "../services/googleAuth.js";
 
 const patientRouter = Router();
 
-patientRouter.get('/',(req,res) => {
-    res.render('patient');
+patientRouter.get('/',async (req,res) => {
+    const doctorArray = await doctor.find({status:"approved"});
+    res.render('patient',{
+        doctors: doctorArray,
+        patientName: "rahul"
+    });
 })
+
+
+patientRouter.get("/auth/google",
+    passport.authenticate("google",{scope: ["profile","email"],session:false})
+)
+
+patientRouter.get("/auth/google/callback",
+    passport.authenticate("google",{session:false,failureRedirect:"/patientPage"}),
+    (req,res) => {
+        res.redirect("/patientPage");
+    }
+)
 
 
 export default patientRouter;
